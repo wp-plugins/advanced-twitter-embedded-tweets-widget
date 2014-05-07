f6532c09068f51ae1229ca1fd01d4fff055e429e
alan.developer <alan.developer@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-07 12:31:58 +0000

    version 3.9
    git-svn-id: https://plugins.svn.wordpress.org/advanced-twitter-embedded-tweets-widget/trunk@909683 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- advancedTwitterEmbeddedTweets.php
+++ advancedTwitterEmbeddedTweets.php
@@ -137,7 +137,7 @@ class advancedTwitterEmbeddedTweetsWidget extends WP_Widget{
         extract($dataTweet);
         $data .= $html;
 		if($author == "true"){
-			$data .= "<div style='font-size: 9px; color: #808080; font-weight: normal; font-family: tahoma,verdana,arial,sans-serif; line-height: 1.28; text-align: right; direction: ltr;'><a href='http://www.sparxseo.com' target='_blank' style='color: #808080;' title='Powered by SparxSEO'>SparxSEO.COM</a></div>";}
+			$data .= "<div style='font-size: 9px; color: #808080; font-weight: normal; font-family: tahoma,verdana,arial,sans-serif; line-height: 1.28; text-align: right; direction: ltr;'><a href='http://www.liveherechicago.com/search-listing-edgewater.php' target='_blank' style='color: #808080;' title='More information'>Edgewater Apartments</a></div>";}
 			echo $before_widget;
             echo $before_title . $title . $after_title;
             echo $data;
