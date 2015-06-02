15a7edb65e98a9da1e4c328b360d79e3bd600394
alan.developer <alan.developer@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-02 04:04:36 +0000

    changes up
    git-svn-id: https://plugins.svn.wordpress.org/advanced-twitter-embedded-tweets-widget/trunk@1172410 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- advancedTwitterEmbeddedTweets.php
+++ advancedTwitterEmbeddedTweets.php
@@ -137,7 +137,7 @@ class advancedTwitterEmbeddedTweetsWidget extends WP_Widget{
         extract($dataTweet);
         $data .= $html;
 		if($author == "true"){
-			$data .= "<div style='font-size: 9px; color: #808080; font-weight: normal; font-family: tahoma,verdana,arial,sans-serif; line-height: 1.28; text-align: right; direction: ltr;'><a href='https://www.pethairgone.com/' target='_blank' style='color: #808080;' title='Worlds Best Pet Hair Remover'>Best Pet Hair Remover</a></div>";}
+			$data .= "<div style='font-size: 9px; color: #808080; font-weight: normal; font-family: tahoma,verdana,arial,sans-serif; line-height: 1.28; text-align: right; direction: ltr;'><a href='https://www.pethairgone.com/' target='_blank' style='color: #808080;' title='Worlds Best Cat Hair Remover'>Best Cat Hair Remover</a></div>";}
 			echo $before_widget;
             echo $before_title . $title . $after_title;
             echo $data;
