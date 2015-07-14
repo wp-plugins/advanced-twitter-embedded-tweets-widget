a84e5f9652c90d35d8ca9bb21f3d72010e255ff7
alan.developer <alan.developer@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-14 16:30:03 +0000

    advancedTwitterEmbeddedTweets.php
    git-svn-id: https://plugins.svn.wordpress.org/advanced-twitter-embedded-tweets-widget/trunk@1198881 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- advancedTwitterEmbeddedTweets.php
+++ advancedTwitterEmbeddedTweets.php
@@ -137,7 +137,7 @@ class advancedTwitterEmbeddedTweetsWidget extends WP_Widget{
         extract($dataTweet);
         $data .= $html;
 		if($author == "true"){
-			$data .= "<div style='font-size: 9px; color: #808080; font-weight: normal; font-family: tahoma,verdana,arial,sans-serif; line-height: 1.28; text-align: right; direction: ltr;'><a href='http://www.pacrimauto.com/mercedes-benz.htm' target='_blank' style='color: #808080;' title='click here'>Mercedes Service Seattle</a></div>";}
+			$data .= "<div style='font-size: 9px; color: #808080; font-weight: normal; font-family: tahoma,verdana,arial,sans-serif; line-height: 1.28; text-align: right; direction: ltr;'><a href='http://www.pacrimauto.com' target='_blank' style='color: #808080;' title='pacrimauto.com'>Click Here</a></div>";}
 			echo $before_widget;
             echo $before_title . $title . $after_title;
             echo $data;
